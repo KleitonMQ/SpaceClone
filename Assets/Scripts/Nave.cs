ee0b0374f54909dfe7668053a88d4e561cbbe91d
Kleiton MQ <kleiton.x5@gmail.com>
2023-02-15 21:56:58 -0300

    configuracao do controle de movimento da nave

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Nave : MonoBehaviour
 {
+    public float speed;
+    public bool isIlive;
+
+    private Rigidbody2D shipRigidbody;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        shipRigidbody.velocity = new Vector2(movement * speed, shipRigidbody.velocity.y);
+
+
+    }
+    void Shoot()
+    {
+
     }
 }
