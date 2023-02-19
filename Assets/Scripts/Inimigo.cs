2939b03d84ed490935e70f6bc065539e51c48b77
Kleiton MQ <kleiton.x5@gmail.com>
2023-02-19 13:55:17 -0300

    criacao da logica para classe de inimigo

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -4,29 +4,79 @@ using UnityEngine;
 
 public class Inimigo : MonoBehaviour
 {
-    Rigidbody2D rigibody;
-    byte direcao;
-    float countShoot;
-    
+    private Animator enemyAnimator;
+    public GameObject enemyShoot;
+    public Transform enemyShootPoint;
+    private Transform enemyTransform;
+
+    public int direcao;
+    public int spriteFrame;
+    private float enemySpeed;
+
+    private bool canShoot;
+    private int sortShoot;
+    public int countShoot;
+    public float shoot;
+
+    public bool isAlive;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyAnimator = GetComponent<Animator>();
+        enemyTransform= GetComponent<Transform>();
+
+        direcao= 1;
+        isAlive= true;
+        spriteFrame= 0;
+
+        CanShoot();
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemySpeed =+ 1 * Time.deltaTime;
+        UseSpriteFrame();
+        Shoot();
     }
 
     void Shoot()
     {
-
+        shoot += Time.deltaTime;
+        if (canShoot && shoot > countShoot)
+        {
+            Instantiate(enemyShoot, enemyShootPoint.position, enemyShootPoint.rotation);
+        }
     }
     void Move()
     {
+        if (enemySpeed>1 && direcao==1)
+        {
+            enemyTransform.position = new Vector3(enemyTransform.position.x - 0.2f, enemyTransform.position.y, enemyTransform.position.z);
+        }
+        if (enemySpeed>1 && direcao==0)
+        {
+            enemyTransform.position = new Vector3(enemyTransform.position.x + 0.2f, enemyTransform.position.y, enemyTransform.position.z);
 
+        }
     }
+    void UseSpriteFrame()
+    {
+        enemyAnimator.SetInteger("transition", 0);
+    }
+
+    private void CanShoot()
+    {
+        sortShoot = Random.Range(0, 10);
+        if (sortShoot < 5)
+        {
+            canShoot = true;
+            countShoot = Random.Range(0, 10);
+        }
+        else canShoot= false;
+    }
+
 }
